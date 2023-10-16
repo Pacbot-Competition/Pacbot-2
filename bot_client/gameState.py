b576fea503536b69398b10604f5553e58f238c53
Ian Henriques <ih2422@princeton.edu>
2023-10-16 13:12:48 -0400

    Implemented functional simulation of actions within Python game state

--- bot_client/gameState.py
+++ bot_client/gameState.py
@@ -13,20 +13,20 @@ from collections import deque
 # Terminal colors for formatting output text
 from terminalColors import *
 
-class GameMode(IntEnum):
+class GameModes(IntEnum):
 	'''
 	Enum of possible game modes
 	'''
 
-	PAUSED = 0
+	PAUSED  = 0
 	SCATTER = 1
-	CHASE = 2
+	CHASE   = 2
 
 # Terminal colors, based on the game mode
 GameModeColors = {
-	GameMode.PAUSED:  DIM,
-	GameMode.CHASE:   YELLOW,
-	GameMode.SCATTER: GREEN
+	GameModes.PAUSED:  DIM,
+	GameModes.CHASE:   YELLOW,
+	GameModes.SCATTER: GREEN
 }
 
 class GhostColors(IntEnum):
@@ -34,31 +34,45 @@ class GhostColors(IntEnum):
 	Enum of possible ghost names
 	'''
 
-	RED = 0
-	PINK = 1
-	CYAN = 2
+	RED    = 0
+	PINK   = 1
+	CYAN   = 2
 	ORANGE = 3
 
-class Direction(IntEnum):
+# Scatter targets for each of the ghosts
+#               R   P   C   O
+SCATTER_ROW = [-3, -3, 31, 31]
+SCATTER_COL = [25,  2, 27,  0]
+
+class Directions(IntEnum):
 	'''
-	Enum of possible directions
+	Enum of possible directions for the Pacman agent
 	'''
 
-	UP = 0
-	LEFT = 1
-	DOWN = 2
+	UP    = 0
+	LEFT  = 1
+	DOWN  = 2
 	RIGHT = 3
+	NONE  = 4
+
+# Directions:        U   L   D   R  None
+D_ROW: list[int] = [-1, -0, +1, +0, +0]
+D_COL: list[int] = [-0, -1, +0, +1, +0]
 
 class Location:
 	'''
 	Location of an entity in the game engine
 	'''
 
-	def __init__(self) -> None:
+	def __init__(self, state) -> None: # type: ignore
 		'''
 		Construct a new location state object
 		'''
 
+		# Relevant game state
+		self.state: GameState = state
+
+		# Row and column information
 		self.rowDir: int  = 0
 		self.row: int     = 32
 		self.colDir: int  = 0
@@ -89,11 +103,16 @@ class Location:
 		# Get the column value (last 6 bits)
 		self.col = col_uint8 & 0x3f
 
-	def at(self, row: int, col: int):
+	def at(self, row: int, col: int) -> bool:
 		'''
 		Determine whether a row and column intersect with this location
 		'''
 
+		# Check the compared position is not an empty location
+		if (row >= 31) or (col >= 28):
+			return False
+
+		# Return whether the rows and columns both match
 		return (self.row == row) and (self.col == col)
 
 	def serialize(self) -> int:
@@ -110,21 +129,67 @@ class Location:
 		# Return the full serialization
 		return (row_uint8 << 8) | (col_uint8)
 
+	def advance(self) -> None:
+		'''
+		Advance this location state for simulating another step transition
+		'''
+
+		# If the current position is out of bounds, ignore it
+		if (self.row > 31) or (self.col > 28):
+			return
+
+		# Calculate the next row and column
+		newRow = self.row + self.rowDir
+		newCol = self.col + self.colDir
+
+		# Move to the next row and column, if applicable
+		if not self.state.wallAt(newRow, newCol):
+			self.row = newRow
+			self.col = newCol
+
+	def setDirection(self, direction: Directions) -> None:
+		'''
+		Given a direction enum object, set the direction of this location
+		'''
+
+		# Set the direction of this location
+		self.rowDir = D_ROW[direction]
+		self.colDir = D_COL[direction]
+
+	def getDirection(self) -> Directions:
+		'''
+		Return a direction enum object corresponding to this location
+		'''
+
+		# Return the matching direction, if applicable
+		for direction in Directions:
+			if self.row == D_ROW[direction] and self.col == D_COL[direction]:
+				return direction
+
+		# Return none if no direction matches
+		return Directions.NONE
+
 class Ghost:
 	'''
 	Location and auxiliary info of a ghost in the game engine
 	'''
 
-	def __init__(self, color: GhostColors) -> None:
+	def __init__(self, color: GhostColors, state) -> None: # type: ignore
 		'''
 		Construct a new ghost state object
 		'''
 
-		# Set the color for this ghost
+		# Relevant game state
+		self.state: GameState = state
+
+		# Ghost information
 		self.color: GhostColors = color
-		self.location: Location = Location()
+		self.location: Location = Location(state) # type: ignore
 		self.frightSteps: int = 0
 		self.spawning: bool = bool(True)
+
+		# (For simulation) Planned next direction the ghost will take
+		self.plannedDirection: Directions = Directions.NONE
 
 	def updateAux(self, auxInfo: int) -> None:
 		'''
@@ -140,6 +205,113 @@ class Ghost:
 		'''
 
 		return (self.spawning << 7) | (self.frightSteps)
+
+	def move(self) -> None:
+		'''
+		Update the ghost's position for simulation purposes
+		'''
+
+		# As an approximation since we don't have enough info, assume the location
+		# of the ghost will not change much if it is spawning (as it might be
+		# trapped in the ghost house) - this holds for short-term simulations into
+		# the future, but feel free to adjust it if you have a better way to
+		# predict how spawning ghosts will behave
+		if self.spawning:
+			return
+
+		# Advance the ghost's location
+		self.location.advance()
+
+		# Set the current direction to the guess of the planned direction
+		self.location.setDirection(self.plannedDirection)
+
+	def guessPlan(self) -> None:
+		'''
+		Use incomplete knowledge of the current game state to predict where the
+		ghosts might aim at the next step
+		'''
+
+		# For the same reason as in move(), ignore spawning ghosts during short-
+		# term projections into the future
+		if self.spawning:
+			return
+
+		# Row and column at the next step
+		nextRow: int = self.location.row + self.location.rowDir
+		nextCol: int = self.location.col + self.location.colDir
+
+		# Pacman row and column
+		pacmanRow: int = self.state.pacmanLoc.row
+		pacmanCol: int = self.state.pacmanLoc.col
+		pacmanRowDir: int = self.state.pacmanLoc.rowDir
+		pacmanColDir: int = self.state.pacmanLoc.colDir
+
+		# Red ghost's location
+		redRow: int = self.state.ghosts[GhostColors.RED].location.row
+		redCol: int = self.state.ghosts[GhostColors.RED].location.col
+
+		# Target row and column
+		targetRow: int = 0
+		targetCol: int = 0
+
+		# Choose a target for the ghost based on its color
+		if self.state.gameMode == GameModes.CHASE:
+
+			# Red targets Pacman
+			if self.color == GhostColors.RED:
+				targetRow = pacmanRow
+				targetCol = pacmanCol
+
+			# Pink targets the space 4 ahead of Pacman
+			elif self.color == GhostColors.PINK:
+				targetRow = pacmanRow + 4 * pacmanRowDir
+				targetRow = pacmanCol + 4 * pacmanColDir
+
+			# Cyan targets the position of red, reflected about the position 2 spaces
+			# ahead of Pacman
+			elif self.color == GhostColors.CYAN:
+				targetRow = 2 * pacmanRow + 4 * pacmanRowDir - redRow
+				targetCol = 2 * pacmanCol + 4 * pacmanColDir - redCol
+
+			# Orange targets Pacman, but only if Pacman is farther than 8 spaces away
+			elif self.color == GhostColors.ORANGE:
+				distSqToPacman = (nextRow - pacmanRow) * (nextRow - pacmanRow) + \
+													(nextCol - pacmanCol) * (nextCol - pacmanCol)
+				targetRow = pacmanRow if (distSqToPacman < 64) else \
+											SCATTER_ROW[GhostColors.ORANGE]
+				targetCol = pacmanCol if (distSqToPacman < 64) else \
+											SCATTER_COL[GhostColors.ORANGE]
+
+		# In scatter mode, each ghost tracks a fixed target at a corner of the maze
+		if self.state.gameMode == GameModes.SCATTER:
+			targetRow = SCATTER_ROW[self.color]
+			targetCol = SCATTER_COL[self.color]
+
+		# Calculate the distance squared to the target, for all 4 moves
+		bestDistSq = 0xfffffff
+		bestDir    = Directions.UP
+		for direction in Directions:
+			if direction != Directions.NONE:
+
+				# Avoid reversals, as ghosts are not typically allowed to reverse
+				if D_ROW[direction] + self.location.rowDir != 0 or \
+					D_COL[direction] + self.location.colDir != 0:
+
+					# Check whether this new location would be valid (not in a wall)
+					newRow = nextRow + D_ROW[direction]
+					newCol = nextCol + D_COL[direction]
+					if not self.state.wallAt(newRow, newCol):
+
+						# Compare the distance squared to the target to the current best;
+						# if it is better, choose it to be the new ghost plan
+						distSqToTarget = (newRow - targetRow) * (newRow - targetRow) + \
+															(newCol - targetCol) * (newCol - targetCol)
+						if distSqToTarget < bestDistSq:
+							bestDir = direction
+							bestDistSq = distSqToTarget
+
+		# Update the best direction to be the plan
+		self.plannedDirection = bestDir
 
 class GameState:
 	'''
@@ -179,7 +351,7 @@ class GameState:
 		self.format += 'B'
 
 		# 1 byte
-		self.gameMode: GameMode = GameMode.PAUSED
+		self.gameMode: GameModes = GameModes.PAUSED
 		self.format += 'B'
 
 		# 2 bytes
@@ -200,15 +372,15 @@ class GameState:
 		self.format += 'B'
 
 		# 4 * 3 bytes = 4 * (2 bytes location + 1 byte aux info)
-		self.ghosts: list[Ghost] = [Ghost(color) for color in GhostColors]
+		self.ghosts: list[Ghost] = [Ghost(color, self) for color in GhostColors]
 		self.format += 'HBHBHBHB'
 
 		# 2 byte location
-		self.pacmanLoc: Location = Location()
+		self.pacmanLoc: Location = Location(self)
 		self.format += 'H'
 
 		# 2 byte location
-		self.fruitLoc: Location = Location()
+		self.fruitLoc: Location = Location(self)
 		self.format += 'H'
 
 		# 31 * 4 bytes = 31 * (32-bit integer bitset)
@@ -311,9 +483,6 @@ class GameState:
 		if self._locked and not lockOverride:
 			return
 
-		print(lockOverride)
-		print(serializedState)
-
 		# Unpack the values based on the format string
 		unpacked: tuple[int, ...] = unpack_from(self.format, serializedState, 0)
 
@@ -352,8 +521,9 @@ class GameState:
 		# Pellet info
 		self.pelletArr = list[int](unpacked)[18:]
 
-		# Display the game state (i.e., terminal printer)
-		# self.display()
+		# Reset our guesses of the planned ghost directions
+		for ghost in self.ghosts:
+			ghost.plannedDirection = Directions.NONE
 
 	def pelletAt(self, row: int, col: int) -> bool:
 		'''
@@ -441,3 +611,83 @@ class GameState:
 
 		# New line at end of display
 		print()
+
+	def safetyCheck(self) -> bool:
+		'''
+		Helper function to check whether Pacman is safe in the current game state
+		(i.e., Pacman is not directly colliding with a non-frightened ghost)
+		'''
+
+		# Retrieve Pacman's coordinates
+		pacmanRow = self.pacmanLoc.row
+		pacmanCol = self.pacmanLoc.col
+
+		# Check for collisions
+		for ghost in self.ghosts:
+			if ghost.location.at(pacmanRow, pacmanCol):
+				if ghost.frightSteps <= 0: # Collision; Pacman loses
+					return False
+				else: # 'Respawn' the ghost
+					ghost.location.row = 32
+					ghost.location.col = 32
+
+		# Otherwise, Pacman is safe
+		return True
+
+	def simulateAction(self, numTicks: int, pacmanDir: Directions) -> bool:
+		'''
+		Helper function to advance the game state (predicting the new ghost
+		positions, modes, and other information) and move Pacman one space in a
+		chosen direction, as a high-level path planning step
+		'''
+
+		# Try to plan the ghost directions if we expect them to be none
+		for ghost in self.ghosts:
+			if ghost.plannedDirection == Directions.NONE:
+				ghost.guessPlan()
+
+		# Loop over every tick
+		for tick in range(numTicks):
+
+			# Keep ticking until an update
+			if (self.currTicks + tick) % self.updatePeriod != 0:
+				continue
+
+			# Update the ghost positions (and reduce frightened steps if applicable)
+			for ghost in self.ghosts:
+				ghost.move()
+
+			# Return if Pacman collides with a non-frightened ghost
+			if not self.safetyCheck():
+				return False
+
+			# Update the mode steps counter, and change the mode if necessary
+			self.modeSteps -= 1
+			if self.modeSteps == 0:
+				if self.gameMode == GameModes.SCATTER:
+					self.gameMode = GameModes.CHASE
+				elif self.gameMode == GameModes.CHASE:
+					self.gameMode = GameModes.SCATTER
+
+			# Guess the next ghost moves (will likely be inaccurate, due to inferring
+			# unknown information from other features of the game state)
+			for ghost in self.ghosts:
+				ghost.guessPlan()
+
+		# If Pacman is not given a direction to move towards, return
+		if pacmanDir == Directions.NONE:
+			return True
+
+		# Set the direction of Pacman, as chosen, and try to move one step
+		self.pacmanLoc.setDirection(pacmanDir)
+		self.pacmanLoc.advance()
+
+		# Return if Pacman collides with a non-frightened ghost
+		if not self.safetyCheck():
+			return False
+
+		# Increment the number of ticks by the chosen amount
+		self.currTicks += numTicks
+
+		# Return that Pacman was safe during this transition
+		return True
