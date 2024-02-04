76e43625c7d7e35a46cd7d733bb222adf658e0d7
FlyN-Nick <43420132+FlyN-Nick@users.noreply.github.com>
2024-02-04 18:14:47 -0500

    Beginnings...

--- bot_client/RL_Stuff.py
+++ bot_client/RL_Stuff.py
@@ -2,24 +2,34 @@ import math
 from gameState import GameState
 from debugServer import DebugServer
 import pathfinding
+import numpy as np
 
 class RLLearn_SARAS():
-    def __init__(self, addr, port):
-        # self.subscriptions = [MsgType.LIGHT_STATE]
-        # super().__init__(addr, port, message_buffers, MsgType, FREQUENCY, self.subscriptions)
-        # self.state = None
-        # self.grid = copy.deepcopy(grid)
+    def __init__(self, addr, port, training=False):
+        self.training = training
 
-        # self.policy = np.zeros((grid.shape, 4))
-        # self.lr = 0.01
-        # self.min_lr = 0.001
-        # self.lr_decay = 0.99
-        # self.gamma = 1
-        # self.eps = 0.1
-        # self.eps_decay = 0.999
-        # self.min_eps = 0.001
+        # let's think about what Q, the 2D array of learned state-action values, should look like
+        # Pacman has 4 possible actions: up, down, left, right
+
+        # The state includes various information,
+        # such as the position of pacman, the position of the ghosts (or lack thereof, cuz they're dead),
+        # the position of pellets, the frightened state of the ghosts
+
+        # 28 * 36 = 1008 possible positions for pacman
+        # 28 * 36 * 2 + 1 = 2017 possible states for the ghosts (position, frightened, or dead)
+        # 28 * 36 = 1008 possible positions for each pellet
+        # add em all up: 1008 + 4*2017 + 1008 = 10084
+        # that's kind of a lot, maybe let's rethink the state space (after all, not all locations are reachable)
+
+        # reference: https://github.com/wrhlearner/PacBot-2023/blob/master/src/Pi/botCode/HighLevelMarkov.py
         return
 
+    """
+    def q_mapper(GameState g):
+        # figure out where a state is in the Q table
+        return
+    """
+    
     def stepping():
         #Updating the new state, the reward for the step, whether pacman is done or not
         return
