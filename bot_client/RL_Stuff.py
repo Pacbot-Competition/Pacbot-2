a98bc55654bbfc95e386025d2d898a10e10879c5
ineskhou <ik2512@columbia.edu>
2024-02-05 20:40:47 -0500

    ssjdk

--- bot_client/RL_Stuff.py
+++ bot_client/RL_Stuff.py
@@ -3,14 +3,14 @@ from gameState import GameState
 from debugServer import DebugServer
 import pathfinding
 
-class RLLearn_SARAS():
-    def __init__(self, addr, port):
+class RLLearn_SARSA():
+    def __init__(self):
         # self.subscriptions = [MsgType.LIGHT_STATE]
         # super().__init__(addr, port, message_buffers, MsgType, FREQUENCY, self.subscriptions)
         # self.state = None
         # self.grid = copy.deepcopy(grid)
 
-        # self.policy = np.zeros((grid.shape, 4))
+        # self.policy = np.zeros((grid.shape, 4))h
         # self.lr = 0.01
         # self.min_lr = 0.001
         # self.lr_decay = 0.99
@@ -34,12 +34,21 @@ class RLLearn_SARAS():
         return
     
     def calculate_reward(state1, state2, action):
+        #takes a board state, the next state and the action and calculates the ending rewards 
         return 0
     
     def action_to_command():
         return
     
     def train():
+        
+        # initialize Q(s,a)
+        # take a random action
+        # update Q(s,a)
+        # choose the action maximises Q or a random action according to Ɛ-greedy function
+        # repeat step 3 and 4 until the game ends
+        # update Q(s,a) where s is the last state before the end, a is the last action taken
+
         return
 
 
